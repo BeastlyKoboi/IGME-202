3905c5780c694ee93b1bf829e5290530fdf9863d
BeastlyKoboi <itsallanflores06@gmail.com>
2022-09-25 21:38:54 -0400

    Created Exercise 5

---
+++ Exercises/Exercise 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject arrow1;  //fill in by Inspector
+    private Arrow1 arrow1_script;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrow1_script = arrow1.GetComponent<Arrow1>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+}
