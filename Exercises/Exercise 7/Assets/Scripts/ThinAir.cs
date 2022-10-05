321766e3e55dc53831d9c42eae527e249542483a
arf7094 <arf7094@g.rit.edu>
2022-10-05 11:45:01 -0400

    Added Exercise 7 to Repo

---
+++ Exercises/Exercise 7/Assets/Scripts/ThinAir.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThinAir : MonoBehaviour
+{
+    // Fields from write-up
+    private float altitude;
+    private Vector3 pos;
+    private Vector3 vel;
+
+    public GameObject thinAirBalloon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 
+        altitude = 5.5f; //so the bottom of the Gondola just touches the ground
+        pos = new Vector3(0, altitude, 0);
+        vel = Vector3.zero;
+        transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Key reading from write-up
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            altitude += .01f;
+            pos.y = altitude; //NOTE: this is more efficient than the statement below
+                              //pos = new Vector3(transform.position.x, altitude, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.Alpha0))
+        {
+            altitude -= .01f; //should check to make sure this doesn’t make altitude < 0
+            pos.y = altitude; //NOTE: this is more efficient than the statement below
+                              //pos = new Vector3(transform.position.x, altitude, transform.position.z);
+        }
+
+        pos += vel * Time.deltaTime;
+        transform.position = pos; //NOTE: transform.position.y cannot be set
+
+    }
+
+    /// <summary>
+    /// Method from write-up to adjust balloon's velocity
+    /// </summary>
+    /// <param name="windSpeed"></param>
+    /// <param name="windDirection"></param>
+    public void WindVelocity(float windSpeed, float windDirection)
+    {
+        vel = windSpeed * (new Vector3(Mathf.Sin(windDirection), 0, Mathf.Cos(windDirection)));
+    }
+}
