fe97c2a3f1a6878b3a9aee7b1301f462166566c5
arf7094 <arf7094@g.rit.edu>
2022-10-19 11:46:08 -0400

    Imported exercise 9 starter

---
+++ Exercises/Exercise 9/Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Collisions.
+/// Component of the Game Manager game object.
+/// Determines collisions between 2 game objects.
+/// </summary>
+public class CollisionDetector: MonoBehaviour 
+{
+    public bool AABBTest(GameObject a, GameObject b)
+	{
+
+        // get horizontal extents of sprite A
+        float aMinX = a.transform.position.x + a.GetComponent<SpriteInfo>().lowLeft.x;
+        float aMaxX = a.transform.position.x + a.GetComponent<SpriteInfo>().upRight.x;
+       
+        // get vertical extents of sprite A
+        float aMinY = a.transform.position.y + a.GetComponent<SpriteInfo>().lowLeft.y;
+        float aMaxY = a.transform.position.y + a.GetComponent<SpriteInfo>().upRight.y;
+
+        // get horizontal extents of sprite B
+        float bMinX = b.transform.position.x + b.GetComponent<SpriteInfo>().lowLeft.x;
+        float bMaxX = b.transform.position.x + b.GetComponent<SpriteInfo>().upRight.x;
+
+        // get vertical extents of sprite B
+        float bMinY = b.transform.position.y + b.GetComponent<SpriteInfo>().lowLeft.y;
+        float bMaxY = b.transform.position.y + b.GetComponent<SpriteInfo>().upRight.y;
+
+
+        /*
+        // Check for a collision using the concept of a separating plane, which if it exists between sprite A and sprite B, means they are not colliding
+
+        if (aMaxX < bMinX) //sprite A is completely to the left of sprite B, so a vertical separating plane exists
+         return false;
+        else if (bMaxX < aMinX) //sprite B is completely to the left of sprite A, so a vertical separating plane exists
+         return false;
+        else if (aMaxY < bMinY) //sprite A is completely below sprite B, so a horizontal separating plane exists
+         return false;
+        else if (bMaxY < aMinY) //sprite B is completely below sprite A, so a horizontal separating plane exists
+         return false;
+        else 
+         return true; // the only remaining alternative is that sprite A and B are colliding
+
+        */
+
+        //this is equivalent to the above, but it's written using ||'s, which do in fact provide a quick "opt out"; as soon as one expression is found to be true, no others are evaluated
+        if ((aMaxX < bMinX) || (bMaxX < aMinX) || (aMaxY < bMinY) || (bMaxY < aMinY))
+            return false;
+        else
+            return true;
+        //
+
+        //Exercise 9 requires that you rewrite the above, using deMorgan's Laws and the four conditions aMaxX > bMinX, bMaxX > aMinX, aMaxY > bMinY, and bMaxY > aMinY
+
+    }
+
+    public bool BoundingCircleTest(GameObject a, GameObject b)
+    {
+        float distance;
+
+        distance = (b.transform.position - a.transform.position).magnitude;
+
+        if (distance > (a.GetComponent<SpriteInfo>().radius + b.GetComponent<SpriteInfo>().radius))
+            return false;
+        else
+            return true;
+    }
+
+    //Exercise 9 requires that you make this routine above more efficient, by using sqrMagnitude instead of magnitude. 
+
+    //The square root calculation is done implicitly by Unity during the calculation of the magnitude value, 
+
+    //The modified test becomes whether:
+
+    //     sqrMagnitude = square of (the distance between two circle centers) is greater than the square of (the sum of the radii of the two circles)
+
+}
---
+++ Exercises/Exercise 9/Assets/Scripts/GameManager.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using UnityEngine.UI; // Note this new line is needed for UI
+using System;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+	public Text scoreText;
+	public Text gameOverText;
+    CollisionDetector collisionDetector;
+    public List<GameObject> rocks;
+    public List<GameObject> antirocks;
+    public GameObject rocket;
+    GameObject antirock;
+    GameObject rock;
+
+    int hits;
+    int antiRocksAwry;
+    int rocksMissed;
+
+
+    bool rockOut;
+
+    public void Start()
+    {
+        hits = 0;
+        antiRocksAwry = 0;
+        rocksMissed = 0;
+        DisplayStats();
+        collisionDetector = gameObject.GetComponent<CollisionDetector>();
+        rocks = new List<GameObject>();
+        antirocks = new List<GameObject>();
+    }
+
+
+    public void Update()
+    {
+        //check to see whether any rock has rolled onto the rocket
+        foreach (GameObject rck in rocks)
+        {
+            if (collisionDetector.AABBTest(rocket, rck))
+               PlayerDied();
+        }
+
+        //check to see whether any antirocks are colliding with any rocks
+        rockOut = false;  //important to note the use of this boolean - see below!
+
+        foreach (GameObject antirck in antirocks )
+        {
+            foreach (GameObject rck in rocks)
+            {
+                if (collisionDetector.BoundingCircleTest(antirck, rck) )
+                {
+                    //Debug.Log("antirock hit rock!");
+                    rockOut = true;
+                    antirock = antirck;
+                    rock = rck;
+                    break;
+                }
+            }
+
+            if (rockOut)
+                break;
+        }
+
+        //it was necessary to use the boolean rockOut to break out of both loops above, since the following removal will alter the two list data structures!
+        if (rockOut)
+        {
+            RemoveRockFromList(rock);
+            RemoveAntiRockFromList(antirock);
+            Destroy(rock);
+            Destroy(antirock);
+            hits++;
+            DisplayStats();
+        }
+
+        //Exercise 9 requires that you remove AntiRocks and Rocks that are out-of-bounds
+        //Suggestion:  use the similar approach as the above, but with separate, not nested, loops since you will process awry AntiRocks separately from the missed Rocks
+    }
+
+    public void AddRockToList(GameObject rock)
+    {
+        rocks.Add(rock);
+    }
+
+    public bool RemoveRockFromList(GameObject rock)
+    {
+        return rocks.Remove(rock);
+    }
+
+    public void AddAntiRockToList(GameObject antirock)
+    {
+        antirocks.Add(antirock);
+    }
+
+    public bool RemoveAntiRockFromList(GameObject antirock)
+    {
+        return antirocks.Remove(antirock);
+    }
+
+
+    //Exercise 9 requires that you display the status regarding Awry AntiRocks and Missed Rocks
+    public void DisplayStats()
+	{
+        scoreText.text = "Hits " + hits.ToString() + "\n" + "AntiRocks Awry  " + antiRocksAwry.ToString() + "\n" + "Rocks Missed " + rocksMissed.ToString();
+    }
+
+    public void PlayerDied()
+	{
+		gameOverText.enabled = true;
+
+		// This freezes the game
+		Time.timeScale = 0;				
+	}
+}
